740719999b353e338f104fbd77e52e2ab87bc056
xiaouhu <542601704@qq.com>
2017-08-29 11:16:20 +0800

    Initial Commit

---
+++ BXHLiveVedio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BXHLiveVedio
+//
+//  Created by 步晓虎 on 2017/8/29.
+//  Copyright © 2017年 步晓虎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
